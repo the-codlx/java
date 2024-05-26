21693d75b8a5d220a8333696388215917d7c2347
the-codlx <lucasgamep.com@gmail.com>
2024-05-26 17:48:11 -0300

    Create teste.java

---
+++ javas()/teste.java
@@ -0,0 +1,10 @@
+public class teste {
+
+    public static void main(String[] args) {
+        System.out.println(" teste");
+
+        System.out.println(" teste 2");
+
+        System.out.println("Começando a usar git");
+    }
+}
